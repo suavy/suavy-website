e88cad082111023d5dd12ebedc22a33ccabe2f0a
Christophe <cdo_outeiro@me.com>
2020-03-27 12:21:37 +0100

    Add helpers to composer.json

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-27 10:41:16.
+    * Generated on 2020-03-27 11:20:56.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
