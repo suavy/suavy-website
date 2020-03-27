0b2bcf49635fa43a4bee5b862d4dd06f8fc3f07d
Christophe <cdo_outeiro@me.com>
2020-03-27 12:17:53 +0100

    Helpers from my-online-resume

---
+++ app/Helpers/DatetimeHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+    function readableStartAndEndDates($startDate, $endDate) {
+        $readableTime = readableTimeBetweenTwoDates($startDate, $endDate);
+        if($startDate && $endDate) {
+            return trans('datetime.readable_start_and_end_dates', ['date_start' => $startDate->format('m/Y'), 'date_end' => $endDate->format('m/Y'),'readable_time' => $readableTime]);
+        }
+        if($startDate) {
+            return trans('datetime.readable_start_date', ['date' => $startDate->format('m/Y'), 'readable_time' => $readableTime]);
+        }
+    }
+
+    function readableTimeBetweenTwoDates($starDate, $endDate = null) {
+        if($endDate == null) $endDate = \Carbon\Carbon::now();
+        $diff = $starDate->diff($endDate);
+        $readableDiff = "";
+        if($diff->y) $readableDiff .= trans_choice('datetime.year', $diff->y, ['value' => $diff->y]);
+        if($diff->y && $diff->m) $readableDiff .= trans('datetime.year_month_separator');
+        if($diff->m) $readableDiff .= trans_choice('datetime.month', $diff->m, ['value' => $diff->m]);
+        return $readableDiff;
+    }
---
+++ app/Helpers/SecurityHelper.php
@@ -0,0 +1,8 @@
+<?php
+
+    function fRobot(string $stringToF) {
+        $stringF = "<span style='unicode-bidi:bidi-override; direction:rtl;'>";
+        $stringF .= strrev($stringToF);
+        $stringF .= "</span>";
+        return $stringF;
+    }
