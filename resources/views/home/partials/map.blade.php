b30c9ac68ccec7c7642faea50570c4cfff0d8a87
Christophe <cdo_outeiro@me.com>
2020-03-30 17:22:01 +0200

    Row map on homepage (with responsive) (test 3)

--- resources/views/home/partials/map.blade.php
+++ resources/views/home/partials/map.blade.php
@@ -1,11 +1,18 @@
 <div class="map-row">
-    <div class="map-title">
-        We are an <span>organization<br/> of freelancers</span>
-        that build<br/> beautiful things around<br/> the world.
+    <div class="map-col">
+        <div class="map-title">
+            We are an <span>organization<br/> of freelancers</span>
+            that build<br/> beautiful things around<br/> the world.
+        </div>
+    </div><div class="map-col">
+        <div class="map-container">
+            <div class="map">
+                @include('home.partials.map-svg')
+            </div>
+            <div class="map-marker" style="top: 15%; left: 47.3%;"></div>
+            <div class="map-marker" style="top: 91%; left: 35%;"></div>
+        </div>
     </div>
-    <div class="map">
-        @include('home.partials.map-svg')
-    </div>
-    <div class="map-marker" style="top: 15%; left: 47.3%;"></div>
-    <div class="map-marker" style="top: 91%; left: 35%;"></div>
+
+
 </div>
