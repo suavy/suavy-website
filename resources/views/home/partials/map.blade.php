954cfcdf6f1bc4d6da17a1cc5ffdd805c6502bff
Christophe <cdo_outeiro@me.com>
2020-03-31 16:25:33 +0200

    add click gesture on markers that open "marker items"

--- resources/views/home/partials/map.blade.php
+++ resources/views/home/partials/map.blade.php
@@ -1,19 +1,39 @@
 <div class="map-row">
     <div class="map-col">
         <div class="map-title">
-            We are an <span>organization<br/> of freelancers</span>
-            that build<br/> beautiful things around<br/> the world.
+            We are an
+            <span>organization of freelancers</span>
+            that build beautiful things around the world.
         </div>
     </div><div class="map-col">
         <div class="map-container">
             <div class="map">
                 @include('home.partials.map-svg')
             </div>
-            <div class="map-marker map-marker--france"></div>
-            <div class="map-marker map-marker--portugal"></div>
-            <div class="map-marker map-marker--brazil"></div>
+            <div class="map-marker map-marker--france">
+                <input class="map-marker__controller" id="map-marker-france" type="checkbox"/>
+                <label class="map-marker__button" for="map-marker-france"><i class="fad fa-times"></i></label>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+            </div>
+            <div class="map-marker map-marker--portugal">
+                <input class="map-marker__controller" id="map-marker-portugal" type="checkbox"/>
+                <label class="map-marker__button" for="map-marker-portugal"><i class="fad fa-times"></i></label>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+            </div>
+            <div class="map-marker map-marker--brazil">
+                <input class="map-marker__controller" id="map-marker-brazil" type="checkbox"/>
+                <label class="map-marker__button" for="map-marker-brazil"><i class="fad fa-times"></i></label>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+                <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
+            </div>
         </div>
     </div>
-
-
 </div>
