a32402a183e001e1ed006bae205e20ff8facd51b
Christophe <cdo_outeiro@me.com>
2020-03-31 18:20:50 +0200

    updated "pulse effect" close button

--- resources/views/home/partials/map.blade.php
+++ resources/views/home/partials/map.blade.php
@@ -12,18 +12,18 @@
             </div>
             <div class="map-marker map-marker--france">
                 <input class="map-marker__controller" id="map-marker-france" type="checkbox"/>
-                <label class="map-marker__button pulse" for="map-marker-france"><i class="fad fa-times"></i></label>
+                <label class="map-marker__button pulse" for="map-marker-france"><i class="fad fa-fw fa-times"></i></label>
                 <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
                 <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
             </div>
             <div class="map-marker map-marker--portugal">
                 <input class="map-marker__controller" id="map-marker-portugal" type="checkbox"/>
-                <label class="map-marker__button pulse" for="map-marker-portugal"><i class="fad fa-times"></i></label>
+                <label class="map-marker__button pulse" for="map-marker-portugal"><i class="fad fa-fw fa-times"></i></label>
                 <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
             </div>
             <div class="map-marker map-marker--brazil">
                 <input class="map-marker__controller" id="map-marker-brazil" type="checkbox"/>
-                <label class="map-marker__button pulse" for="map-marker-brazil"><i class="fad fa-times"></i></label>
+                <label class="map-marker__button pulse" for="map-marker-brazil"><i class="fad fa-fw fa-times"></i></label>
                 <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
                 <div class="map-marker__item"><img src="https://picsum.photos/60"/></div>
             </div>
