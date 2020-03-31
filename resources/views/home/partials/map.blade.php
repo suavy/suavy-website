b8c99269b2a934f5339abeff9797938798e06aff
Christophe <cdo_outeiro@me.com>
2020-03-31 11:42:19 +0200

    add markers on map + update design

--- resources/views/home/partials/map.blade.php
+++ resources/views/home/partials/map.blade.php
@@ -9,8 +9,9 @@
             <div class="map">
                 @include('home.partials.map-svg')
             </div>
-            <div class="map-marker" style="top: 15%; left: 47.3%;"></div>
-            <div class="map-marker" style="top: 91%; left: 35%;"></div>
+            <div class="map-marker map-marker--france"></div>
+            <div class="map-marker map-marker--portugal"></div>
+            <div class="map-marker map-marker--brazil"></div>
         </div>
     </div>
 
