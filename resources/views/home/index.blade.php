83d5dfe87f8bc3bbbf8889ab81986129535224b3
Christophe <cdo_outeiro@me.com>
2020-04-09 12:06:18 +0200

    contact row partial

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.main')
 @section('content')
+    <!-- Good rows -->
     @include('home.partials.map')
     @include('home.partials.services')
-
     @include('home.partials.team')
+    @include('home.partials.contact')
 
+    <!-- Bad rows, need update to became more beautiful -->
     @include('home.partials.match')
-
     @include('home.partials.clients')
 
 @endsection
---
+++ resources/views/home/partials/contact.blade.php
@@ -0,0 +1,13 @@
+<div class="home__row home__row--contact">
+    <h2 class="title">Form Contact Row</h2>
+    <div class="contact">
+        <form class="contact__form">
+            <!-- todo
+                    - What type of project do you need ? (checkbox multiple)
+                    - Delivery time (checkbox multiple)
+                    - Budget range (checkbox multiple)
+                    - Name,Email,Message (text,text,textarea)
+            -->
+        </form>
+    </div>
+</div>
