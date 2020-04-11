2c2ee052cede76704250aefc288daa1392a8e275
Christophe <cdo_outeiro@me.com>
2020-04-11 15:28:44 +0200

    added emojis to form (test)

--- resources/views/home/partials/contact.blade.php
+++ resources/views/home/partials/contact.blade.php
@@ -1,10 +1,10 @@
 <div class="home__row home__row--contact">
     <div class="contact">
         <x-form>
-            <x-inputs.checkboxes label="What type(s) of services do you need ?"/>
-            <x-inputs.checkboxes label="Delivery Time"/>
-            <x-inputs.checkboxes label="Budget Range"/>
-            <x-inputs.text name="a" label="Nom" :params="['size' => 6, 'placeholder' => 'Your name']"/>
+            <x-inputs.checkboxes label="What type(s) of services do you need ? 🤔"/>
+            <x-inputs.checkboxes label="Delivery Time 🐢"/>
+            <x-inputs.checkboxes label="Budget Range 💰"/>
+            <x-inputs.text name="a" label="Name" :params="['size' => 6, 'placeholder' => 'Your name']"/>
             <x-inputs.text name="b" label="Email" :params="['size' => 6, 'placeholder' => 'email@example.com']"/>
             <x-inputs.textarea name="c" label="Message" :params="['placeholder' => 'Your most detailed message ❤']"/>
         </x-form>
