6cdf16fbb20be300d33168d7c4c4ba9098f42790
Christophe <cdo_outeiro@me.com>
2020-04-11 15:21:50 +0200

    better form design

--- resources/views/components/inputs/checkboxes.blade.php
+++ resources/views/components/inputs/checkboxes.blade.php
@@ -1,7 +1,9 @@
 <div class="form-group">
     <label class="label">{{ $label }}</label>
-    <x-inputs.checkbox name="c1" label="Label here"/>
-    <x-inputs.checkbox name="c2" label="Label here"/>
-    <x-inputs.checkbox name="c3" label="Label here"/>
-    <x-inputs.checkbox name="c4" label="Label here"/>
+    <div class="checkboxes">
+        <x-inputs.checkbox name="c1" label="Lorem ipsum dolor sit amet"/>
+        <x-inputs.checkbox name="c2" label="Lorem ipsum dolor sit amet"/>
+        <x-inputs.checkbox name="c3" label="Lorem ipsum dolor sit amet"/>
+        <x-inputs.checkbox name="c4" label="Lorem ipsum dolor sit amet"/>
+    </div>
 </div>
--- resources/views/home/partials/contact.blade.php
+++ resources/views/home/partials/contact.blade.php
@@ -4,9 +4,9 @@
             <x-inputs.checkboxes label="What type(s) of services do you need ?"/>
             <x-inputs.checkboxes label="Delivery Time"/>
             <x-inputs.checkboxes label="Budget Range"/>
-            <x-inputs.text name="a" label="Nom" :params="['size' => 6]"/>
-            <x-inputs.text name="b" label="Email" :params="['size' => 6]"/>
-            <x-inputs.textarea name="c" label="Message" :params="['placeholder' => 'Type your message here']"/>
+            <x-inputs.text name="a" label="Nom" :params="['size' => 6, 'placeholder' => 'Your name']"/>
+            <x-inputs.text name="b" label="Email" :params="['size' => 6, 'placeholder' => 'email@example.com']"/>
+            <x-inputs.textarea name="c" label="Message" :params="['placeholder' => 'Your most detailed message ❤']"/>
         </x-form>
     </div>
 </div>
