4f710d9149588ce1b1b324cdc32099ef59e42bf8
Matthieu <kassian.matthieu@gmail.com>
2020-04-23 16:10:19 +0200

    animate services

--- resources/views/home/partials/services.blade.php
+++ resources/views/home/partials/services.blade.php
@@ -12,8 +12,9 @@
                         @endforeach
                     </div>
                 </div>@endforeach
-            <div class="services-view-more js-show-services">
-                See <a href="javascript:;" class="link link--underline">everything we do</a>.
+            <div class="services-view-more">
+                <span class="js-services-show-more">See <a href="javascript:;" class="link link--underline">everything we do</a>.</span>
+                <span style="display: none" class="js-services-show-more-or-contact"><span class="link link--underline js-services-to-contact-us" >Contact us</span> or <span class="js-services-show-less link link--underline" >see less</span>.</span>
             </div>
         </div>
     </div>
@@ -39,14 +40,40 @@
 
         let countServices = $('#service-categories').data('count');
 
-        $(".js-show-services").click(function () {
-            console.log('click');
+        $(".js-services-show-more").click(function () {
             for(i=1; i<=countServices; i++){
                 console.log('.js-service-'+i);
                 $('.js-service-'+i).show();
-                animateCSS('.js-service-'+i, 'jackInTheBox');
+                animateCSS('.js-service-'+i, 'fadeInLeft');
             }
+            animateCSS('.js-services-show-more', 'fadeOutRight',function () {
+                $('.js-services-show-more').hide();
+                $('.js-services-show-more-or-contact').show();
+                animateCSS('.js-services-show-more-or-contact', 'fadeInLeft');
+            });
         })
+
+        $('.js-services-show-less').click(function () {
+            for(i=1; i<=countServices; i++){
+                let e = $('.js-service-'+i);
+                animateCSS('.js-service-'+i, 'fadeOutRight',function(){
+                    $('.js-services-show-more-or-contact').hide();
+                    $('.js-services-show-more').show();
+                    e.hide();
+                });
+            }
+            animateCSS('.js-services-show-more-or-contact', 'fadeOutRight',function () {
+                $('.js-services-show-more').show();
+                $('.js-services-show-more-or-contact').hide();
+                animateCSS('.js-services-show-more', 'fadeInLeft');
+            });
+        });
+
+        $('.js-services-to-contact-us').on('click', function() {
+            var speed = 500; // Durée de l'animation (en ms)
+            $('html, body').animate( { scrollTop: $("#contact").offset().top }, speed ); // Go
+            return false;
+        });
 
     </script>
 @endpush
