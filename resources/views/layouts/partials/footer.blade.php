1e892b32ccb45f323d5a7d89674e49e3e76cacb6
Christophe <cdo_outeiro@me.com>
2020-04-24 15:23:32 +0200

    update css contact form for debug

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,6 +5,6 @@
         <li><a href="#" class="link" target="_blank" rel="nofollow noreferrer noopener"><i class="fab fa-twitter"></i> Twitter</a></li>
     </ul>
     <div class="footer__about">
-        <i class="fad fa-code color-primary-color"></i> with <i class="fad fa-heart color-red"></i> by us and <a href="https://github.com/suavy/suavy-website" class="link link--underline" target="_blank" rel="nofollow noreferrer noopener">open source</a>.
+        <i class="fad fa-code color-primary"></i> with <i class="fad fa-heart color-red"></i> by us and <a href="https://github.com/suavy/suavy-website" class="link link--underline" target="_blank" rel="nofollow noreferrer noopener">open source</a>.
     </div>
 </div>
