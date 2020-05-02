b4ade2219d91e5922481f11b069190daaff313bf
Christophe <cdo_outeiro@me.com>
2020-05-02 15:34:25 +0200

    wording en base

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -2,9 +2,26 @@
 
 return [
     'map' => [
-        'title' => 'We are an <span>organization of freelancers</span> that build beautiful things around the world.',
+        'title' => 'We are a <span>community of freelancers</span> that build beautiful things around the world.',
     ],
     'services' => [
-        'title' => 'Expertise',
+        'closed' => 'See everything we do.',
+        'opened' => 'A good match ? Get in touch now !'
     ],
+    'contact' => [
+        'services_label' => 'Which service(s) are you interested in ?',
+        'deliveries_label' => 'Delivery Time',
+        'budgets_label' => 'Budget Range',
+        'name_label' => 'Name',
+        'name_placeholder' => '',
+        'email_label' => 'Email',
+        'email_placeholder' => '',
+        'message_label' => 'Message',
+        'message_placeholder' => 'Your most detailed message here ❤',
+        'button_name' => 'Your project starts now',
+    ],
+    'clients' => [
+        'title' => 'They trusted us',
+    ],
+    'footer' => '</> with <3 by us and open source',
 ];
