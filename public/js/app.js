4bdb7845304eef646b6ff51891261889a0bd9c17
breno romeiro <bsromeiro@gmail.com>
2020-05-02 11:12:07 -0300

    added translated wording

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/suavy-website/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/suavy-website/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/breno/laravel/code/suavy-website/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/breno/laravel/code/suavy-website/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
