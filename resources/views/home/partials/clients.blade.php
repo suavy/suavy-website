944f8cb38c1cf5f1b0f52a2a0b89d512b0024891
Christophe <cdo_outeiro@me.com>
2020-05-18 10:30:04 +0200

    remove temp logos

--- resources/views/home/partials/clients.blade.php
+++ resources/views/home/partials/clients.blade.php
@@ -1,26 +1,8 @@
 <div class="home__row home__row--clients bg-secondary">
     <div class="clients-title">Companies we worked with</div>
     <div class="clients">
-        {{--
         @foreach($projects as $project)
             <img src="{{ $project->company_logo }}"/>
-        @endforeach--}}
-        @foreach($projects->take(1) as $project)
-            <div class="client">
-                <img src="{{ asset('images/temp_logos/remplafrance.svg') }}" />
-            </div>
-            <div class="client">
-                <img src="{{ asset('images/temp_logos/carrefour.svg') }}" />
-            </div>
-            <div class="client">
-                <img src="{{ asset('images/temp_logos/bdbuzz.png') }}" />
-            </div>
-            <div class="client">
-                <img src="{{ asset('images/temp_logos/victor-charles.png') }}" />
-            </div>
-            <div class="client">
-                <img src="{{ asset('images/temp_logos/lookaya.png') }}" />
-            </div>
         @endforeach
     </div>
 </div>
