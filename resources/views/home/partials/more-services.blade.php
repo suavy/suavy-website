e39e56bfc9588f05f1361b534d1300ec3192a280
Pedro Arthur <pedro.art.vel@gmail.com>
2020-05-25 18:17:54 -0300

    Better code styling, better services styling

--- resources/views/home/partials/more-services.blade.php
+++ resources/views/home/partials/more-services.blade.php
@@ -2,37 +2,37 @@
     <div class="show-services">Check more of our services</div>
 </div> -->
 <div class="services-more invisible">
-    <h2>Our services</h2>
+    <h2>About our services</h2>
     <h3>Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Quam elementum pulvinar etiam non. Eu consequat.</h3>
     <div class="services-category">
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__design">
+            <div class="services-avatar services-avatar__design"><i class="fal fa-pencil-ruler"></i></div>
+            <h3>Design</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__api">
+            <div class="services-avatar services-avatar__api"><i class="fal fa-chess-rook-alt"></i></div>
+            <h3>API Integration</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__webdev">
+            <div class="services-avatar services-avatar__webdev"><i class="fal fa-browser"></i></div>
+            <h3>Web Development</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__mobile">
+            <div class="services-avatar services-avatar__mobile"><i class="fal fa-mobile"></i></div>
+            <h3>Mobile Develoment</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__data">
+            <div class="services-avatar services-avatar__data"><i class="fal fa-database"></i></div>
+            <h3>Data Management</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
-        <div class="services-categories">
-            <div class="services-avatar"><i class="fas fa-angle-double-left"></i></div>
-            <h3>Webdesign</h3>
+        <div class="services-categories services_categories__marketing">
+            <div class="services-avatar services-avatar__marketing"><i class="fal fa-lightbulb-on"></i></div>
+            <h3>Marketing</h3>
             <p>Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.</p>
         </div>
     </div>
